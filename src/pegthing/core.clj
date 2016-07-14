39ab8b8a9a75fe976ac90d5fa6257df48732c687
Francis Luong (Franco) <fluong@salesforce.com>
2016-07-14 15:31:11 -0400

    row-tri, row-num, and connect

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -17,3 +17,26 @@
   "Is the number triangular? e.g. from set [1, 3, 6, 10, 15]"
   [n]
   (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns a row number the position belongs to:
+    row 1: 1
+    row 2: 2, 3
+    etc..."
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos src-pos nei-pos dest-pos]
+  (if (<= dest-pos max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] nei-pos))
+            board
+            [[src-pos dest-pos] [dest-pos src-pos]])))
+
--- test/pegthing/core_test.clj
+++ test/pegthing/core_test.clj
@@ -2,12 +2,38 @@
   (:require [clojure.test :refer :all]
             [pegthing.core :refer :all]))
 
+(def the-triangular-five [1 3 6 10 15])
 (deftest tri-test
   (testing "taking from tri"
-    (is (= [1 3 6 10 15] (take 5 tri)))
+    (is (= the-triangular-five (take 5 tri)))
     (is (= [1 3 6 10] (take 4 tri)))))
 
 (deftest triangular-test
   (testing "triangularity of known numbers"
-    (is (triangular? 6))
+    (is (every? triangular? the-triangular-five))
     (is not (triangular? 5))))
+
+(deftest row-tri-test
+  (testing "row-tri values"
+    (is (= 1 (row-tri 1)))
+    (is (= 3 (row-tri 2)))
+    (is (= 6 (row-tri 3)))
+    (is (= 10 (row-tri 4)))
+    (is (= 15 (row-tri 5)))))
+
+(deftest row-num-test
+  (testing "row-num for selected values"
+    (is (= 1 (row-num 1)))
+    (is (= 2 (row-num 2)))
+    (is (= 3 (row-num 5)))
+    (is (= 5 (row-num 14)))))
+
+(deftest connect-test
+  (testing "establishment of board position connections"
+    (is (= {1 {:connections {4 2}}
+            4 {:connections {1 2}}} (connect {} 15 1 2 4))))
+  (testing "assoc-in/get-in deep map work"
+    (def x-map {:cookie {:monster {:vocals "Finntroll"}}})
+    (is (= x-map (assoc-in {} [:cookie :monster :vocals] "Finntroll")))
+    (is (= {:vocals "Finntroll"} (get-in x-map [:cookie :monster])))
+    (is (= {1 {:connections {4 2}}} (assoc-in {} [1 :connections 4] 2)))))
