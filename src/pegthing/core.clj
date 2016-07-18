bb1e4bcd27b7a2ea8aabe2525e8f53596d217e7f
Francis Luong (Franco) <fluong@salesforce.com>
2016-07-18 14:19:22 -0400

    up to new-board

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -9,7 +9,7 @@
   ([] (tri* 0 1))
   ([sum n]
    (let [new-sum (+ sum n)]
-    (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
 (def tri (tri*))
 
@@ -38,5 +38,51 @@
     (reduce (fn [new-board [p1 p2]]
               (assoc-in new-board [p1 :connections p2] nei-pos))
             board
-            [[src-pos dest-pos] [dest-pos src-pos]])))
+            [[src-pos dest-pos] [dest-pos src-pos]])
+    board))
 
+(defn connect-right
+  "Given a board and a position, form connections toward E if possible"
+  [board max-pos pos]
+  (let [nei-pos (inc pos)
+        dest-pos (inc nei-pos)]
+    (if-not (or (triangular? nei-pos) (triangular? pos))
+      (connect board max-pos pos nei-pos dest-pos)
+      board)))
+
+(defn connect-down-left
+  "Given a board and position, form connections toward SW, if possible"
+  [board max-pos pos]
+  (let [row (row-num pos)
+        nei-pos (+ row pos)
+        dest-pos (+ 1 row nei-pos)]
+    (connect board max-pos pos nei-pos dest-pos)))
+
+(defn connect-down-right
+  "Given a board and position, form connections toward SE, if possible"
+  [board max-pos pos]
+  (let [row (row-num pos)
+        nei-pos (+ 1 row pos)
+        dest-pos (+ 2 row nei-pos)]
+    (connect board max-pos pos nei-pos dest-pos)))
+
+(defn add-pos
+  "reduces on a vector of function applying each in turn to build a board
+    - Pegs the position and then adds connections toward E, SW, SE"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board fn-connection-creation]
+              (fn-connection-creation new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right])))
+
+(defn new-board
+  "Create a new peg board given number of rows
+    - first create initial empty board and get max-pos
+    - reduce range of 1 to max-pos+1 and reduce using add-pos"
+  [row-count]
+  (let [initial-board {:row-count row-count}
+        max-pos (row-tri row-count)]
+    (reduce (fn [acc-board pos] (add-pos acc-board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
--- test/pegthing/core_test.clj
+++ test/pegthing/core_test.clj
@@ -31,9 +31,65 @@
 (deftest connect-test
   (testing "establishment of board position connections"
     (is (= {1 {:connections {4 2}}
-            4 {:connections {1 2}}} (connect {} 15 1 2 4))))
+            4 {:connections {1 2}}} (connect {} 15 1 2 4)))
+    (is (= {} (connect {} 15 15 16 17))))
   (testing "assoc-in/get-in deep map work"
     (def x-map {:cookie {:monster {:vocals "Finntroll"}}})
     (is (= x-map (assoc-in {} [:cookie :monster :vocals] "Finntroll")))
     (is (= {:vocals "Finntroll"} (get-in x-map [:cookie :monster])))
+    ; # assoc-in in-hash key-list value
     (is (= {1 {:connections {4 2}}} (assoc-in {} [1 :connections 4] 2)))))
+
+(deftest connect-right-test
+  (testing "connect-right"
+    (is (= {} (connect-right {} 15 3)))
+    (is (= {} (connect-right {} 15 1)))
+    (is (= {4 {:connections {6 5}}
+            6 {:connections {4 5}}} (connect-right {} 15 4)))))
+
+(deftest connect-down-right-test
+  (testing "connect-down-right"
+    (is (= {} (connect-down-right {} 15 15)))
+    (is (= {1 {:connections {6 3}}
+            6 {:connections {1 3}}} (connect-down-right {} 15 1)))
+    (is (= {2 {:connections {9 5}}
+            9 {:connections {2 5}}} (connect-down-right {} 15 2)))
+    (is (= {3  {:connections {10 6}}
+            10 {:connections {3 6}}} (connect-down-right {} 15 3)))))
+
+(deftest connect-down-left-test
+  (testing "connect-down-left"
+    (is (= {} (connect-down-left {} 15 15)))
+    (is (= {1 {:connections {4 2}}
+            4 {:connections {1 2}}} (connect-down-left {} 15 1)))
+    (is (= {2 {:connections {7 4}}
+            7 {:connections {2 4}}} (connect-down-left {} 15 2)))
+    (is (= {3 {:connections {8 5}}
+            8 {:connections {3 5}}} (connect-down-left {} 15 3)))))
+
+(deftest add-pos-test
+  (testing "add-pos"
+    (def x-board {1 {:pegged true, :connections {4 2, 6 3}}
+                  4 {:connections {1 2}}
+                  6 {:connections {1 3}}})
+    (is (= x-board (add-pos {} 15 1)))))
+
+(deftest new-board-test
+  (testing "new board"
+    (def x-board {:row-count 5
+                  1 {:connections {4 2, 6 3}, :pegged true}
+                  2 {:connections {7 4, 9 5}, :pegged true}
+                  3 {:connections {8 5, 10 6}, :pegged true}
+                  4 {:connections {1 2, 6 5, 11 7, 13 8}, :pegged true}
+                  5 {:connections {12 8, 14 9}, :pegged true}
+                  6 {:connections {1 3, 4 5, 13 9, 15 10}, :pegged true}
+                  7 {:connections {2 4, 9 8}, :pegged true}
+                  8 {:connections {3 5, 10 9}, :pegged true}
+                  9 {:connections {2 5, 7 8}, :pegged true}
+                  10 {:connections {3 6, 8 9}, :pegged true}
+                  11 {:connections {4 7, 13 12}, :pegged true}
+                  12 {:connections {5 8, 14 13}, :pegged true}
+                  13 {:connections {4 8, 6 9, 11 12, 15 14}, :pegged true}
+                  14 {:connections {5 9, 12 13}, :pegged true}
+                  15 {:connections {6 10, 13 14}, :pegged true}})
+    (is (= x-board (new-board 5)))))
